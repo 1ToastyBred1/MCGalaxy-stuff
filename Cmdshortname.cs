7cddae4478471c07a786afbcfa514effbafc4b48
ToastyBred <sempheer@gmail.com>
2024-08-05 19:34:13 +0300

    Update Cmdshortname.cs

--- Cmdshortname.cs
+++ Cmdshortname.cs
@@ -13,31 +13,31 @@ public class CmdShortname : Command
 	public override void Use(Player p, string message)
 	{
 		if (message.Length == 0) 
-        {
-        	PlayerOperations.SetNick(p, p.name, p.ColoredName);
-        	return; 
-        }
-        
-        string nick = Colors.StripUsed(message);
-        
-        if (nick.Length <= 3)
-        {
-        	p.Message("&eShortened name must be 3 characters or more.");
-        	return;
-        }
-        
-        if (!p.name.Contains(nick))
-        {
-        	p.Message("&eYour shortened name must be a part of your original name.");
-        	return;
-        }
-        
-        PlayerOperations.SetNick(p, p.name, message);
+	        {
+	        	PlayerOperations.SetNick(p, p.name, p.ColoredName);
+	        	return; 
+	        }
+	        
+	        string nick = Colors.StripUsed(message);
+	        
+	        if (nick.Length <= 3)
+	        {
+	        	p.Message("&eShortened name must be 3 characters or more.");
+	        	return;
+	        }
+	        
+	        if (!p.name.Contains(nick))
+	        {
+	        	p.Message("&eYour shortened name must be a part of your original name.");
+	        	return;
+	        }
+	        
+	        PlayerOperations.SetNick(p, p.name, message);
 	}
 
 	public override void Help(Player p)
 	{
 		p.Message("&a/Shortname [New nick]");
-        p.Message("&eAllows you to shorten your name. Your new name must be a part of your original name. You are allowed to add colors.");
+        	p.Message("&eAllows you to shorten your name. Your new name must be a part of your original name. You are allowed to add colors.");
 	}
 }
